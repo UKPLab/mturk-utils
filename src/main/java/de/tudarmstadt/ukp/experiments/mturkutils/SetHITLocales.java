85d93caf5d8076b1b79f919aff94ad2fd981a484
Tristan Miller <psychonaut@nothingisreal.com>
2017-06-14 16:33:53 +0200

    Update package name

--- src/main/java/de/tudarmstadt/ukp/experiments/mturkutils/SetHITLocales.java
+++ src/main/java/de/tudarmstadt/ukp/experiments/mturkutils/SetHITLocales.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package de.tudarmstadt.ukp.experiments.snoopy.tf3.crowdsourcing;
+package de.tudarmstadt.ukp.experiments.mturkutils;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
